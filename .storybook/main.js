6de359c10dfac5c69ffa103d0303c1e2bfd4bc85
zenparsing <zenparsing@gmail.com>
2022-08-09 12:20:00 -0400

    Use environment variable for overriding storybook paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,18 @@
-module.exports = {
-  stories: [
+function getStories () {
+  if (process.env['STORYBOOK_STORY_PATH']) {
+    return process.env['STORYBOOK_STORY_PATH']
+      .split(/\s+/)
+      .map((path) => `../${path}`)
+  }
+
+  return [
     '../components/**/stories/*.tsx',
     '../components/**/*.stories.tsx'
-  ],
+  ]
+}
+
+module.exports = {
+  stories: getStories(),
   addons: [
     '@storybook/addon-knobs',
     '@storybook/addon-essentials'
