6af2485d0f535b2be99a7ccf3d1cd3f91f4ad83b
Terry Mancey <terry.mancey@icloud.com>
2022-10-01 17:29:56 -0500

    Add l10n locale util

--- components/l10n/common/locale_util.h
+++ components/l10n/common/locale_util.h
@@ -8,16 +8,51 @@
 
 #include <string>
 
-namespace brave_l10n {
+namespace brave_l10n::icu {
 
-std::string GetLanguageCode(
-    const std::string& locale);
+class ScopedLocaleForTesting final {
+ public:
+  // Override the ICU locale for testing. |locale| should be in the format
+  // <language>_<country_code> or <language>-<country_code>, e.g. "en_US" or
+  // "en-001". Restores the previous locale on destruction.
+  explicit ScopedLocaleForTesting(const std::string& locale);
 
-std::string GetCountryCode(
-    const std::string& locale);
+  ~ScopedLocaleForTesting();
+};
 
-std::u16string GetLocalizedResourceUTF16String(int resource_id);
+// Set the ICU locale for testing. |locale| should be in the format
+// <language>_<country_code> or <language>-<country_code>, e.g. "en_US" or
+// "en-001".
+void SetLocaleForTesting(const std::string& locale);
 
-}  // namespace brave_l10n
+// Returns the ICU style C locale in <language>_<country_code> format, e.g.
+// "en_US".
+std::string GetLocale();
+
+// Returns a lowercase two-letter ISO 639-1 language code for the ICU locale,
+// see https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes.
+std::string GetLanguageCode();
+
+// Returns a lowercase two-letter ISO 639-1 language code for the given locale,
+// see https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes. |locale| should be
+// in the format <language>_<country_code> or <language>-<country_code>, e.g.
+// "en_US" or "en-001".
+std::string GetLanguageCode(const std::string& locale);
+
+// Returns an uppercase two-letter ISO 3166-1 alpha-2 country code or UN-M49
+// area code for the ICU locale. See
+// https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2, and
+// https://en.wikipedia.org/wiki/UN_M49.
+std::string GetCountryCode();
+
+// Returns an uppercase two-letter ISO 3166-1 alpha-2 country code or UN-M49
+// area code for the given locale. Use |IsUNM49AreaCode| to check if the
+// country code is a UN M49 area code. |locale| should be in the format
+// <language>_<country_code> or <language>-<country_code>, e.g. "en_US" or
+// "en-001". See https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2, and
+// https://en.wikipedia.org/wiki/UN_M49.
+std::string GetCountryCode(const std::string& locale);
+
+}  // namespace brave_l10n::icu
 
 #endif  // BRAVE_COMPONENTS_L10N_COMMON_LOCALE_UTIL_H_
